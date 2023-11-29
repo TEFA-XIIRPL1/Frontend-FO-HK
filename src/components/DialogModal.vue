55370b3407b114ae27809236fd19b7878318024e
Diaz-adrianz <diazadrianz191205@gmail.com>
2023-11-29 09:40:25 +0700

    feat: setup dialog components

---
+++ src/components/DialogModal.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-dialog v-model="isOpen">
+    <q-card style="width: 700px; max-width: 80vw">
+      <q-toolbar>
+        <q-avatar>
+          <img src="../assets/img/lingian-logo-colored.png" />
+        </q-avatar>
+
+        <q-toolbar-title>{{ title }}</q-toolbar-title>
+
+        <q-btn flat round dense icon="close" @click="closeDialog" />
+      </q-toolbar>
+
+      <q-card-section>
+        <slot></slot>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    open: {
+      type: Boolean,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    isOpen: {
+      get() {
+        return this.open
+      },
+      set(value) {
+        this.$emit('update:open', value)
+      }
+    }
+  },
+  methods: {
+    closeDialog() {
+      this.isOpen = false
+    }
+  }
+}
+</script>
--- src/pages/FO/fragments/GuestForm.vue
+++ src/pages/FO/fragments/GuestForm.vue
@@ -1,11 +1,64 @@
 <template>
   <h5>Fragment: Guest Form goes here...</h5>
+  <q-btn color="primary" @click="paymentDetailDialog = true">Open Dialog</q-btn>
+  <DialogModal
+    :open="paymentDetailDialog"
+    @update:open="paymentDetailDialog = $event"
+    title="Payment Detail"
+  >
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, iste pariatur! Id iusto
+    earum esse quidem nulla obcaecati tempore est sed culpa. Nesciunt quod, neque et sequi in
+    consectetur quo sint ratione obcaecati repudiandae sapiente mollitia, atque maiores quaerat.
+    Officia laboriosam sed assumenda magnam natus, totam mollitia esse quasi delectus illo sequi
+    vitae iure perspiciatis fugit eaque ut nemo quidem, culpa asperiores officiis! Mollitia omnis
+    obcaecati reiciendis rerum neque modi laudantium at beatae optio, velit quaerat fugit doloribus,
+    libero officiis odio! Quod nesciunt magnam voluptatibus ea perspiciatis, quia possimus
+    doloremque nemo eligendi ullam vel, necessitatibus magni tempora architecto minus fuga unde
+    error accusantium autem facere itaque maiores nostrum. Fugiat dolorum voluptatibus repudiandae
+    excepturi non perspiciatis quae autem repellendus incidunt quidem! Lorem ipsum dolor sit amet
+    consectetur adipisicing elit. Consectetur, iste pariatur! Id iusto earum esse quidem nulla
+    obcaecati tempore est sed culpa. Nesciunt quod, neque et sequi in consectetur quo sint ratione
+    obcaecati repudiandae sapiente mollitia, atque maiores quaerat. Officia laboriosam sed assumenda
+    magnam natus, totam mollitia esse quasi delectus illo sequi vitae iure perspiciatis fugit eaque
+    ut nemo quidem, culpa asperiores officiis! Mollitia omnis obcaecati reiciendis rerum neque modi
+    laudantium at beatae optio, velit quaerat fugit doloribus, libero officiis odio! Quod nesciunt
+    magnam voluptatibus ea perspiciatis, quia possimus doloremque nemo eligendi ullam vel,
+    necessitatibus magni tempora architecto minus fuga unde error accusantium autem facere itaque
+    maiores nostrum. Fugiat dolorum voluptatibus repudiandae excepturi non perspiciatis quae autem
+    repellendus incidunt quidem! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Consectetur, iste pariatur! Id iusto earum esse quidem nulla obcaecati tempore est sed culpa.
+    Nesciunt quod, neque et sequi in consectetur quo sint ratione obcaecati repudiandae sapiente
+    mollitia, atque maiores quaerat. Officia laboriosam sed assumenda magnam natus, totam mollitia
+    esse quasi delectus illo sequi vitae iure perspiciatis fugit eaque ut nemo quidem, culpa
+    asperiores officiis! Mollitia omnis obcaecati reiciendis rerum neque modi laudantium at beatae
+    optio, velit quaerat fugit doloribus, libero officiis odio! Quod nesciunt magnam voluptatibus ea
+    perspiciatis, quia possimus doloremque nemo eligendi ullam vel, necessitatibus magni tempora
+    architecto minus fuga unde error accusantium autem facere itaque maiores nostrum. Fugiat dolorum
+    voluptatibus repudiandae excepturi non perspiciatis quae autem repellendus incidunt quidem!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, iste pariatur! Id iusto
+    earum esse quidem nulla obcaecati tempore est sed culpa. Nesciunt quod, neque et sequi in
+    consectetur quo sint ratione obcaecati repudiandae sapiente mollitia, atque maiores quaerat.
+    Officia laboriosam sed assumenda magnam natus, totam mollitia esse quasi delectus illo sequi
+    vitae iure perspiciatis fugit eaque ut nemo quidem, culpa asperiores officiis! Mollitia omnis
+    obcaecati reiciendis rerum neque modi laudantium at beatae optio, velit quaerat fugit doloribus,
+    libero officiis odio! Quod nesciunt magnam voluptatibus ea perspiciatis, quia possimus
+    doloremque nemo eligendi ullam vel, necessitatibus magni tempora architecto minus fuga unde
+    error accusantium autem facere itaque maiores nostrum. Fugiat dolorum voluptatibus repudiandae
+    excepturi non perspiciatis quae autem repellendus incidunt quidem!
+  </DialogModal>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import DialogModal from 'src/components/DialogModal.vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-  name: 'GuestForm'
+  name: 'GuestForm',
+  data() {
+    return {
+      paymentDetailDialog: false
+    }
+  },
+  components: { DialogModal }
 })
 </script>
