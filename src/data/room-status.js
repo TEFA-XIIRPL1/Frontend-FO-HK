16b815d07fa3228855fa80499eaa3839420dc285
refi <refihikman06@gmail.com>
2023-12-06 20:34:28 +0700

    feat: HK-Room Plan Page (ongoing)

---
+++ src/data/room-status.js
@@ -0,0 +1,21 @@
+export const roomStatus = [
+  [
+    { status: 'VC', desc: 'Vacant Clean', bg_color: '#ffffff', text_color: '#000000' },
+    { status: 'VD', desc: 'Vacant Dirty', bg_color: '#10780a', text_color: '#ffffff' },
+    { status: 'VCU', desc: 'Vacant Clean Unchecked', bg_color: '#02feff', text_color: '#000000' }
+  ],
+  [
+    { status: 'OC', desc: 'Occupied Clean', bg_color: '#ffffff', text_color: '#0000f1' },
+    { status: 'OD', desc: 'Occupied Dirty', bg_color: '#0000f1', text_color: '#ffffff' },
+    { status: 'ED', desc: 'Expected Departure', bg_color: '#fffc06', text_color: '#f10000' }
+  ],
+  [
+    { status: 'DnD', desc: 'Do not Disturb', bg_color: '#850179', text_color: '#ffffff' },
+    { status: 'OO', desc: 'Out of Order', bg_color: '#000000', text_color: '#f10000' },
+    { status: 'OM', desc: 'Out of Market', bg_color: '#000000', text_color: '#f10000' }
+  ],
+  [
+    { status: 'VIP', desc: 'VIP', bg_color: '#ffffff', text_color: '#f10000' },
+    { status: 'INCOGNITO', desc: 'INCOGNITO', bg_color: '#f10000', text_color: '#ffffff' }
+  ]
+]
--- src/pages/HK/reports/RoomPlan.vue
+++ src/pages/HK/reports/RoomPlan.vue
@@ -1,17 +1,261 @@
 <template>
-  <q-page class="flex flex-center">
-    <h3>Room Plan</h3>
+  <q-page class="flex flex-center page-padding">
+    <div style="width: 100%; position: relative; overflow: hidden">
+      <q-img src="/src/assets/svg/roomplan_bg.svg" />
 
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia nam consectetur assumenda ad
-      expedita eligendi id distinctio tempore accusamus iste!
-    </p>
+      <!-- LEFT ROOMS -->
+      <div
+        style="
+          position: absolute;
+          left: 3.5%;
+          top: 31%;
+          width: 100%;
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+        "
+      >
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 15%;
+            height: 12.7%;
+            border-bottom-style: solid;
+            border-right-style: solid;
+            border-top-style: solid;
+            border-left-style: solid;
+          "
+          label="107"
+          :style="`background-color: ${roomData[6].bg_color}; color: ${roomData[6].text_color}`"
+        >
+          <RoomPlanDropdown :room="107" @status="handleStatus" />
+        </q-btn-dropdown>
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 15%;
+            height: 12.7%;
+            border-bottom-style: solid;
+            border-right-style: solid;
+            border-left-style: solid;
+          "
+          label="106"
+          :style="`background-color: ${roomData[5].bg_color}; color: ${roomData[5].text_color}`"
+        >
+          <RoomPlanDropdown :room="106" @status="handleStatus" />
+        </q-btn-dropdown>
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 15%;
+            height: 12.7%;
+            border-bottom-style: solid;
+            border-right-style: solid;
+            border-left-style: solid;
+          "
+          label="105"
+          :style="`background-color: ${roomData[4].bg_color}; color: ${roomData[4].text_color}`"
+        >
+          <RoomPlanDropdown :room="105" @status="handleStatus" />
+        </q-btn-dropdown>
+      </div>
+
+      <!-- TOP ROOMS -->
+      <div
+        style="position: absolute; left: 17.7%; top: 6%; width: 100%; height: 100%; display: flex"
+      >
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 21%;
+            height: 18%;
+            border-bottom-style: solid;
+            border-right-style: solid;
+            border-left-style: solid;
+            border-top-style: solid;
+          "
+          label="108"
+          :style="`background-color: ${roomData[7].bg_color}; color: ${roomData[7].text_color}`"
+        >
+          <RoomPlanDropdown :room="108" @status="handleStatus" />
+        </q-btn-dropdown>
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 21%;
+            height: 18%;
+            border-bottom-style: solid;
+            border-right-style: solid;
+            border-top-style: solid;
+          "
+          label="109"
+          :style="`background-color: ${roomData[8].bg_color}; color: ${roomData[8].text_color}`"
+        >
+          <RoomPlanDropdown :room="109" @status="handleStatus" />
+        </q-btn-dropdown>
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 21%;
+            height: 18%;
+            border-bottom-style: solid;
+            border-right-style: solid;
+            border-top-style: solid;
+          "
+          label="110"
+          :style="`background-color: ${roomData[9].bg_color}; color: ${roomData[9].text_color}`"
+        >
+          <RoomPlanDropdown :room="110" @status="handleStatus" />
+        </q-btn-dropdown>
+      </div>
+
+      <!-- BOTTOM ROOMS -->
+      <div
+        style="position: absolute; left: 17.5%; top: 75%; width: 100%; height: 100%; display: flex"
+      >
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 12.9%;
+            height: 19.3%;
+            border-top-style: solid;
+            border-right-style: solid;
+            border-bottom-style: solid;
+          "
+        ></q-btn-dropdown>
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 12.9%;
+            height: 19.3%;
+            border-top-style: solid;
+            border-right-style: solid;
+            border-bottom-style: solid;
+          "
+          label="104"
+          :style="`background-color: ${roomData[3].bg_color}; color: ${roomData[3].text_color}`"
+        >
+          <RoomPlanDropdown :room="104" @status="handleStatus" />
+        </q-btn-dropdown>
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 12.9%;
+            height: 19.3%;
+            border-top-style: solid;
+            border-right-style: solid;
+            border-bottom-style: solid;
+          "
+          label="103"
+          :style="`background-color: ${roomData[2].bg_color}; color: ${roomData[2].text_color}`"
+        >
+          <RoomPlanDropdown :room="103" @status="handleStatus" />
+        </q-btn-dropdown>
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 12.9%;
+            height: 19.3%;
+            border-top-style: solid;
+            border-right-style: solid;
+            border-bottom-style: solid;
+          "
+          label="102"
+          :style="`background-color: ${roomData[1].bg_color}; color: ${roomData[1].text_color}`"
+        >
+          <RoomPlanDropdown :room="102" @status="handleStatus" />
+        </q-btn-dropdown>
+        <q-btn-dropdown
+          class="room-box"
+          style="
+            width: 12.9%;
+            height: 19.3%;
+            border-top-style: solid;
+            border-right-style: solid;
+            border-bottom-style: solid;
+          "
+          label="101"
+          :style="`background-color: ${roomData[0].bg_color}; color: ${roomData[0].text_color}`"
+        >
+          <RoomPlanDropdown :room="101" @status="handleStatus" />
+        </q-btn-dropdown>
+      </div>
+    </div>
+
+    <div class="full-width">VC</div>
   </q-page>
 </template>
+
 <script>
+import RoomPlanDropdown from 'src/components/RoomPlanDropdown.vue'
 import { defineComponent } from 'vue'
+import { roomStatus } from 'src/data/room-status.js'
 
 export default defineComponent({
-  name: 'RoomPlanPage'
+  name: 'RoomPlanPage',
+  components: { RoomPlanDropdown },
+  data() {
+    return {
+      roomStatus,
+      roomData: [
+        { room: 101, status: '', bg_color: '', text_color: '' },
+        { room: 102, status: '', bg_color: '', text_color: '' },
+        { room: 103, status: '', bg_color: '', text_color: '' },
+        { room: 104, status: '', bg_color: '', text_color: '' },
+        { room: 105, status: '', bg_color: '', text_color: '' },
+        { room: 106, status: '', bg_color: '', text_color: '' },
+        { room: 107, status: '', bg_color: '', text_color: '' },
+        { room: 108, status: '', bg_color: '', text_color: '' },
+        { room: 109, status: '', bg_color: '', text_color: '' },
+        { room: 110, status: '', bg_color: '', text_color: '' }
+      ]
+    }
+  },
+  methods: {
+    handleStatus(data) {
+      const index = this.roomData.findIndex((room) => room.room === data.room)
+
+      if (index !== -1) {
+        this.roomData.splice(index, 1, { ...data, room: data.room })
+      } else {
+        this.roomData.push({ ...data, room: data.room })
+      }
+    }
+  }
 })
 </script>
+
+<style>
+.room-box {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2vw;
+  font-weight: bold;
+  border-width: 0.4vw;
+  border-color: black;
+  border-top-style: none;
+  border-bottom-style: none;
+  border-left-style: none;
+  border-right-style: none;
+}
+
+.room-box .q-icon {
+  display: none;
+}
+
+.page-padding {
+  padding: 0 0.5rem;
+}
+@media (min-width: 480px) {
+  .page-padding {
+    padding: 0 2rem;
+  }
+}
+@media (min-width: 1024px) {
+  .page-padding {
+    padding: 0 4rem;
+  }
+}
+</style>
